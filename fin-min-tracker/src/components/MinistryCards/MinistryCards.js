21ed1786ae5fcc4b2302c33e7bdad2b05df3260d
Niranjan420 <niranjanrajesh02@gmail.com>
2020-12-22 22:01:15 +0530

    Updated Carousel 1.0

--- fin-min-tracker/src/components/MinistryCards/MinistryCards.js
+++ fin-min-tracker/src/components/MinistryCards/MinistryCards.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import sky from '../../assets/sky.jpeg'
+import styles from './MinistryCards.module.css'
 
 export default function MinistryCards(props) {
   return (
-    <div>
-      <p style={{"color":"white"}}>text is here</p>
+    <div className={styles.Card}>
+      <h2 className={styles.Title}>{props.name}</h2>
+      <p className={styles.Link}>Overview</p>
+      <p className={styles.Link}>Budget Breakdown</p>
+      <p className={styles.Link}>Transactions</p>
     </div>
+
   )
 }
