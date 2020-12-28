54c435ae04e7e3ad32c35346a7b7c143c5442524
Niranjan420 <niranjanrajesh02@gmail.com>
2020-12-28 12:09:17 +0530

    DetailsTable Data done

--- fin-min-tracker/src/components/SecondModal/SecondModal.js
+++ fin-min-tracker/src/components/SecondModal/SecondModal.js
@@ -16,16 +16,70 @@ const CollapseRow = (props) => {
         <td>{props.dataVal.date}</td>
       </tr>
       {toggleClick &&
-      <tr style={{"align": "ce"}}>
-      <h1 id="title">React Dynamic Table</h1>
-      <table id="personnel">
-        <tbody>Some</tbody>
-      </table>
-      <hr />
-      <table id="status">
-        <tbody>thing</tbody>
-      </table>
-    </tr>
+        <tr style={{ "align": "ce" }}>
+          <table id="summary">
+            <h4>Summary</h4>
+            <tbody>
+              <tr>
+                <th>Details</th>
+                <th>Description</th>
+              </tr>
+              <tr>
+                <td>{props.dataVal.details}</td>
+                <td>{props.dataVal.description}</td>
+              </tr>
+              <tr>
+                <th>Date</th>
+                <th>Total</th>
+              </tr>
+              <tr>
+                <td>{props.dataVal.date}</td>
+                <td>{props.dataVal.total}</td>
+              </tr>
+            </tbody>
+          </table>
+          <hr />
+          <table id="general-info">
+            <h4>General Info</h4>
+            <tbody>
+              <tr>
+                <th>Type</th>
+                <th>Category</th>
+              </tr>
+              <tr>
+                <td>{props.dataVal.type}</td>
+                <td>{props.dataVal.category}</td>
+              </tr>
+              <tr>
+                <th>Tag</th>
+                <th>Bundle</th>
+              </tr>
+              <tr>
+                <td>{props.dataVal.tags}</td>
+                <td>{props.dataVal.bundle}</td>
+              </tr>
+              <tr>
+                <th>Added at</th>
+              </tr>
+              <tr>
+                <td>{props.dataVal.addedAt}</td>
+              </tr>
+            </tbody>
+          </table>
+          <table id="track-transaction">
+            <h4>Track Transaction</h4>
+            <tbody>
+              <tr>
+                <th>Status</th>
+                <th>Link</th>
+              </tr>
+              <tr>
+                <td>{props.dataVal.status}</td>
+                <td>{props.dataVal.link}</td>
+              </tr>
+            </tbody>
+          </table>
+        </tr>
       }
     </tbody>
   );
@@ -56,21 +110,22 @@ export default function Modal(props) {
       })
   }, []);
 
-  const dataKeys = data? Object.keys(data): null;
-  const dataValue = data; 
+  const dataKeys = data ? Object.keys(data) : null;
+  const dataValue = data;
   console.log(data)
 
-  const dynamicRows = dataKeys?dataKeys.map(index => {
-            return (
-              <CollapseRow dataVal={dataValue[index]}/>
-            )}):<></>
+  const dynamicRows = dataKeys ? dataKeys.map(index => {
+    return (
+      <CollapseRow dataVal={dataValue[index]} />
+    )
+  }) : <></>
 
   return (
     <div>
-      {data && 
+      {data &&
         <div className="table-responsive">
-        <table className="table table-borderless">
-          <thead className='thead-dark'>
+          <table className="table table-borderless">
+            <thead className='thead-dark'>
               <tr >
                 <th >#</th>
                 <th>Tag</th>
@@ -80,19 +135,19 @@ export default function Modal(props) {
               </tr>
             </thead>
             {dynamicRows}
-        </table>
-      </div>
-      }
-      
-      {!data && 
-      <div>
-        <div className="Modal">
-          Nothing to show here
+          </table>
         </div>
-        <Backdrop clicked={props.clicked} />
-      </div>
       }
-  </div>
+
+      {!data &&
+        <div>
+          <div className="Modal">
+            Nothing to show here
+        </div>
+          <Backdrop clicked={props.clicked} />
+        </div>
+      }
+    </div>
 
   )
 
