1601c3df74414d6fb393fedb35a090da463e3d6e
Niranjan420 <niranjanrajesh02@gmail.com>
2021-01-05 16:21:24 +0530

    Added Expandable Row Structure

--- fin-min-tracker/src/components/SortingTable/SortingTable.js
+++ fin-min-tracker/src/components/SortingTable/SortingTable.js
@@ -1,10 +1,23 @@
 import React, { useMemo } from 'react'
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, useExpanded } from 'react-table'
 import { format } from 'date-fns'
 
 
 export default function SortingTable(props) {
   const Columns = useMemo(() => [
+    {
+      // Make an expander cell
+      Header: () => null, // No header
+      id: 'expander', // It needs an ID
+      Cell: ({ row }) => (
+        // Use Cell to render an expander for each row.
+        // We can use the getToggleRowExpandedProps prop-getter
+        // to build the expander.
+        <span {...row.getToggleRowExpandedProps()}>
+          {row.isExpanded ? '👇' : '👉'}
+        </span>
+      ),
+    },
     {
       Header: 'Date',
       accessor: 'date',
@@ -36,17 +49,32 @@ export default function SortingTable(props) {
   const tableInstance = useTable({
     columns: Columns,
     data: Data
-  }, useSortBy)
+  }, useSortBy, useExpanded)
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
-    prepareRow } = tableInstance
+    prepareRow,
+    visibleColumns,
+    state: { expanded } } = tableInstance
+  const renderRowSubComponent = React.useCallback(
+    ({ row }) => (
+      <pre
+        style={{
+          fontSize: '10px',
+        }}
+      >
+        <code>{JSON.stringify({ values: row.values }, null, 2)}</code>
+      </pre>
+    ),
+    []
+  )
   return (
     <table {...getTableProps()}>
       <thead>
         {headerGroups.map((headerGroup) => (
+
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
               <th {...column.getHeaderProps(column.getSortByToggleProps())}>
@@ -57,6 +85,10 @@ export default function SortingTable(props) {
               </th>
             ))}
           </tr>
+
+
+
+
         ))}
 
       </thead>
@@ -65,12 +97,22 @@ export default function SortingTable(props) {
           rows.map(row => {
             prepareRow(row)
             return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return (<td {...cell.getCellProps()}>{cell.render('Cell')}</td>)
-                })}
+              <>
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (<td {...cell.getCellProps()}>{cell.render('Cell')}</td>)
+                  })}
 
-              </tr>
+                </tr>
+                {row.isExpanded ? (
+                  <tr>
+                    <td colSpan={visibleColumns.length}>
+                      {renderRowSubComponent({ row })}
+                    </td>
+                  </tr>
+                ) : null}
+              </>
+
             )
           })
         }
