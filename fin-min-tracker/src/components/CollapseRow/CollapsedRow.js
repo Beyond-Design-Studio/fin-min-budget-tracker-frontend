28385217b51eaa60b12dc2ed8db414aa7abb1053
Niranjan420 <niranjanrajesh02@gmail.com>
2021-01-08 15:45:12 +0530

    SecondModal height issue fixed

--- fin-min-tracker/src/components/CollapseRow/CollapsedRow.js
+++ fin-min-tracker/src/components/CollapseRow/CollapsedRow.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { format } from 'date-fns'
 
 import CollapseRowTranscations from "../CollapseRowTransactions/CollapseRowTransactions";
 
@@ -21,12 +22,12 @@ const CollapseRow = (props) => {
   console.log("qwertyuqwertyui", props)
   return (
     <tbody>
-      <tr style={{cursor: "pointer"}} className="collapse-head" onClick={() => setToggleClick(!toggleClick)}>
+      <tr style={{ cursor: "pointer" }} className="collapse-head" onClick={() => setToggleClick(!toggleClick)}>
         <td>{toggleClick ? "▼" : "▶"}</td>
         <td>{props.dataVal ? props.dataVal.tags : data.tags}</td>
         <td>{props.dataVal ? props.dataVal.total : data.total}</td>
         <td>{props.dataVal ? props.dataVal.details : data.details}</td>
-        <td>{props.dataVal ? props.dataVal.date : data.date}</td>
+        <td>{props.dataVal ? format(new Date(props.dataVal.date), 'dd/MM/yyyy') : data.date}</td>
       </tr>
       {toggleClick &&
         <tr>
