a33343c5f4b11e63461931a4689ea6b7983a74bf
Argha Chakrabarty <ac380012@gmail.com>
2021-01-09 13:44:29 +0530

    final-update

--- fin-min-tracker/src/containers/Footer/Footer.js
+++ fin-min-tracker/src/containers/Footer/Footer.js
@@ -8,8 +8,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <a target="_blanck" href="https://beyonddesignstudio.com" alt="beyond design studio"><img className="react-logo" src={beyond_logo} alt="Beyond Design" /></a>
       <a target="_blanck" href="https://sg.ashoka.edu.in" alt="student govt"><img className="sg_logo" src={sg_logo} alt="student govt" /></a>
+      <a target="_blanck" href="https://beyonddesignstudio.com" alt="beyond design studio"><img className="react-logo" src={beyond_logo} alt="Beyond Design" /></a>
     </div>
   )
 }
